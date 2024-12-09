d888c9e8507cecc41a1da87a0c4de1177072422b
Alif Faturrohman <aliffaturrohman11@gmail.com>
2024-12-09 12:10:38 +0700

    fix deployment for azure

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+from backend.ml_model_v2.app_v2 import app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- backend/ml_model_v2/app_v2.py
+++ backend/ml_model_v2/app_v2.py
@@ -1,35 +1,37 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
-from models_v2 import load_resources, predict_category  # Mengimpor fungsi dari models_v2.py
+from backend.ml_model_v2.models_v2 import load_resources, predict_category
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Menerima data inputan dalam format JSON
     data = request.get_json()
-    
+
     # Mengambil title dan description dari request body
     title = data.get('title')
     description = data.get('description')
-    
+
     # Memastikan input ada
     if not title or not description:
         return jsonify({'error': 'Title and description are required'}), 400
-    
+
     # Memuat sumber daya (tokenizer, model, label encoder)
     embedding_model = load_resources()  # Anda menggunakan model embedding di sini
-    
+
     # Prediksi kategori berdasarkan judul dan deskripsi
     predicted_category, predicted_probability = predict_category(title, description, embedding_model)
-    
+
     # Mengembalikan hasil prediksi dalam format JSON
     return jsonify({
         'predicted_category': predicted_category,
         'probability': predicted_probability.tolist()  # Mengubah numpy array ke list
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Port diatur berdasarkan variabel lingkungan 'PORT' untuk kompatibilitas dengan Azure
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
